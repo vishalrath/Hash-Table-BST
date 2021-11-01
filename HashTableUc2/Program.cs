13129eec98f4545b77b414e0e1668021787de146
vishal <vishalrathod7038@gmail.com>
2021-11-02 01:10:48 +0530

    [Vishal] find frequency of large paragraph

---
+++ HashTableUc2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HashTableUc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CountWordFrequency("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations");
+            Console.ReadLine();
+        }
+        public static void CountWordFrequency(string sentence)
+        {
+            MyHashCode<string, int> hashtable = new MyHashCode<string, int>(10);
+            string[] words = sentence.Split(' ');
+            foreach (string word in words)
+            {
+                if (hashtable.Exists(word))
+                {
+                    hashtable.Add(word, hashtable.get(word) + 1);
+
+                }
+                else
+                {
+                    hashtable.Add(word, 1);
+                }
+            }
+            Console.WriteLine("Displaying after add operations");
+            hashtable.Display();
+            Console.WriteLine("=======================================");
+        }
+    }
+}
