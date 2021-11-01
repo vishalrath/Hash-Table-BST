c0f9dc3d270b087c260acd79c791892b68a411f5
vishal <vishalrathod7038@gmail.com>
2021-11-02 00:54:27 +0530

    [Vishal] find frequency

---
+++ HashTableUc1/MyHashCode.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableUc1
+{
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+    class MyHashCode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyHashCode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+
+        }
+
+        protected int getArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V get(K key)
+        {
+            int position = getArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+
+                }
+            }
+            return default(V);
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = getArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            if (linkedlist.Count != 0)
+            {
+                foreach (KeyValue<K, V> item1 in linkedlist)
+                {
+                    if (item1.Key.Equals(key))
+                    {
+                        Remove(key);
+                        break;
+                    }
+                }
+
+            }
+            linkedlist.AddLast(item);
+        }
+
+        public void Remove(K key)
+        {
+            int position = getArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> founditem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    founditem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedlist.Remove(founditem);
+            }
+
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+
+            }
+            return linkedlist;
+
+        }
+
+        public bool Exists(K key)
+        {
+            int position = getArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return true;
+
+                }
+            }
+            return false;
+        }
+
+        public void Display()
+        {
+            foreach (var linkedlist in items)
+            {
+                if (linkedlist != null)
+                {
+                    foreach (var element in linkedlist)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " = " + element.Value);
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
+
---
+++ HashTableUc1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTableUc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CountWordFrequency("To be or not to be said ok not to be said ok to");
+            Console.ReadLine();
+        }
+        public static void CountWordFrequency(string sentence)
+        {
+            MyHashCode<string, int> hashtable = new MyHashCode<string, int>(10);
+            string[] words = sentence.Split(' ');
+            foreach (string word in words)
+            {
+                if (hashtable.Exists(word))
+                {
+                    hashtable.Add(word, hashtable.get(word) + 1);
+
+                }
+                else
+                {
+                    hashtable.Add(word, 1);
+                }
+            }
+            Console.WriteLine("Displaying after add operations");
+            hashtable.Display();
+            Console.WriteLine("=======================================");
+        }
+    }
+}
+
+
